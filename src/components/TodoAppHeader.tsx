7ad3fc52c1e6d46f0fa4d5a241f123e64506108a
Banderos14 <anton.shyshenko@gmail.com>
2026-05-13 11:22:13 +0200

    task_solution

---
+++ src/components/TodoAppHeader.tsx
@@ -0,0 +1,48 @@
+/* eslint-disable jsx-a11y/control-has-associated-label */
+import cn from 'classnames';
+import React from 'react';
+
+type Props = {
+  hasTodos: boolean;
+  allTodosCompleted: boolean;
+  inputRef: React.RefObject<HTMLInputElement>;
+  title: string;
+  disabled: boolean;
+  onTitleChange: (title: string) => void;
+  onSubmit: (event: React.FormEvent<HTMLFormElement>) => void;
+};
+
+export const TodoAppHeader: React.FC<Props> = ({
+  hasTodos,
+  allTodosCompleted,
+  inputRef,
+  title,
+  disabled,
+  onTitleChange,
+  onSubmit,
+}) => (
+  <header className="todoapp__header">
+    {hasTodos && (
+      <button
+        type="button"
+        className={cn('todoapp__toggle-all', {
+          active: allTodosCompleted,
+        })}
+        data-cy="ToggleAllButton"
+      />
+    )}
+
+    <form onSubmit={onSubmit}>
+      <input
+        ref={inputRef}
+        data-cy="NewTodoField"
+        type="text"
+        className="todoapp__new-todo"
+        placeholder="What needs to be done?"
+        value={title}
+        disabled={disabled}
+        onChange={event => onTitleChange(event.target.value)}
+      />
+    </form>
+  </header>
+);
